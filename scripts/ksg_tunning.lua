02ee2129f03c28fe021dd046254491d213a3c329
chengzhi <chengzhi@kuaishou.com>
2024-02-19 19:03:38 +0800

    属性命名以及多语言适配

--- scripts/ksg_tunning.lua
+++ scripts/ksg_tunning.lua
@@ -1,4 +1,53 @@
 
+KSG_CONF = {
+    IS_CH = true
+}
+
+
+KSG_POWERS = {
+    USER = {
+        HUNGER = "hunger",
+        SANITY = "sanity",
+        HEALTH = "health",
+        PICKER = "picker",
+        FARMER = "farmer",
+        HUNTER = "hunter",
+    },
+
+    ITEM = {
+        -- 武器
+        DAMAGE = "damage",
+        SPEEDR = "speedr" ,
+        AOEATK = "aoeatk",
+        MAXUSE = "maxuse",
+        BLOODR = "bloodr" ,
+        -- 工具
+        CHOPER = "choper",
+        MINEER = "mineer",
+        -- 衣服、护甲
+        WATERR = "waterr",
+        TMPRET = "tmpret",
+        DAPNER = "dapner",
+        ABSORB = "absorb",
+    },
+
+    MONS = {
+        HEALTH = "mon_health",
+        SPEEDR = "mon_speedr",
+        CRTATK = "mon_crtatk",
+        DAMAGE = "mon_damage",
+        RELATK = "mon_relatk",
+        ICEEPL = "mon_iceepl",
+        SANAUR = "mon_sanaur",
+        ABSORB = "mon_absorb",
+        KNOCKR = "mon_konckr",
+        STEALR = "mon_stealr",
+        BLOODR = "mon_bloodr",
+        THORNR = "mon_thornr",
+    }
+}
+
+
 KSG_EVENTS = {
     POWER_ATTACH = "KSG_POWER_ATTACH",
     TASK_FINISH  = "KSG_TASK_FINISH" ,
